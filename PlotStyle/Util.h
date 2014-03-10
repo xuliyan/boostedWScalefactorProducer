673b9f3244bd1da2d585e96001a8ac5eeb02d8e5
Raffaele <raffaele.gerosa@cern.ch>
2014-03-11 00:55:22 +0100

    updates

--- PlotStyle/Util.h
+++ PlotStyle/Util.h
@@ -41,27 +41,29 @@ using namespace std;
 using namespace ROOT;
 using namespace TMath;
 
-#ifndef UTIL
-#define UTIL
+void draw_error_band(RooAbsData&, RooAbsPdf&,  RooRealVar&, RooFitResult*, RooPlot*, 
+                     const int & = 6, const std::string & ="F", const int & = 100, const int & = 2000);
 
-void draw_error_band( RooAbsData&,  RooAbsPdf&,  RooRealVar&, RooFitResult*, RooPlot*, const int & = 6, const std::string & ="F", const int & = 100, const int & = 2000);
+void draw_error_band_pull(RooAbsData &, RooAbsPdf &, RooRealVar &, RooFitResult*, RooPlot*, 
+                          const int & = 6, const int & = 100, const int & = 2000);
 
-void draw_error_band_pull(RooAbsData &,  RooAbsPdf &,  RooRealVar &, RooFitResult *, RooPlot *, const int & = 6, const int & = 100, const int & = 2000);
+TGraphAsymmErrors* draw_error_band_ws(RooAbsData &, RooAbsPdf &, RooRealVar &, RooArgList &, RooWorkspace &, RooPlot *,  
+                                      const std::string & = "", const int & = 6, const std::string & = "F", const int & = 100, const int & = 2000);
 
-TGraphAsymmErrors* draw_error_band_ws(RooAbsData &, RooAbsPdf &, const std::string &, RooRealVar &, RooArgList &,  RooWorkspace &, RooPlot *, const int & = 6, const std::string & = "F", const int & = 100, const int & = 2000);
+void draw_error_band_extendPdf(RooAbsData &, RooAbsPdf&, RooFitResult*, RooPlot*, 
+                               const int & = 6, const std::string & = "F", const int & = 100,  const int & = 2000);
 
-void draw_error_band_extendPdf(RooAbsData &,  RooAbsPdf&, RooFitResult*, RooPlot*, const int & = 6, const std::string & = "F", const int & = 100,  const int & = 2000);
+void draw_error_band_extendPdf_pull(RooAbsData&, RooAbsPdf&, RooFitResult*, RooPlot*, 
+                                    const int & = 6, const int & = 100, const int & = 2000);
 
-void draw_error_band_extendPdf_pull( RooAbsData&,  RooAbsPdf&, RooFitResult*, RooPlot*, const int & = 6, const int & = 100, const int & = 2000);
+void draw_error_band_Decor( std::string,std::string, RooArgList &,  RooWorkspace &, RooRealVar &, RooPlot *, 
+                            const int & = 6, const std::string & = "F", const int & = 100, const int & = 2000);
 
-void draw_error_band_Decor( const std::string &, const std::string & , RooArgList &,  RooWorkspace &, RooRealVar &, RooPlot *, const int & = 6, const std::string & = "F", const int & = 100, const int & = 2000);
 
+void draw_error_band_shape_Decor(std::string,std::string,  RooArgList &,  RooWorkspace &, double &, RooPlot *, 
+                                 const int & = 6, const std::string & = "F", const int & = 3013, const std::string & = "", const int & = 100, const int & = 2000);
 
-void draw_error_band_shape_Decor( const std::string &, const std::string &,  RooArgList &,  RooWorkspace &, double &, RooPlot *, const int & = 6, const std::string & = "F", const int & = 3013, const std::string & = "", const int & = 100, const int & = 2000);
-
-double Calc_error_extendPdf( RooAbsData &,  RooExtendPdf &, RooFitResult *, const std::string &, const int & = 2000);
+double Calc_error_extendPdf( RooAbsData &,  RooExtendPdf &, RooFitResult *, const std::string & = "", const int & = 2000);
 
 /// useful for couting analysis
-double Calc_error( const std::string & rpdfname, const std::string & xaxis_name,  RooArgList &,  RooWorkspace &, const std::string &, const int & = 2000);
-
-#endif
+double Calc_error( std::string, std::string,  RooArgList &,  RooWorkspace &, const std::string & = "", const int & = 2000);
