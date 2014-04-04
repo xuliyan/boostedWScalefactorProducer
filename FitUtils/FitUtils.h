3beb5c81ca687425839c9626bb75ec17a776f9e3
Raffaele <raffaele.gerosa@cern.ch>
2014-04-04 15:17:38 +0200

    updates for 0+1 jet bin

--- FitUtils/FitUtils.h
+++ FitUtils/FitUtils.h
@@ -40,8 +40,8 @@ const std::string & = "em", const std::string & = "HP", const int & = 0);
 
 void fit_mlvj_model_single_MC(RooWorkspace*, const std::string & ="", const std::string & = " ", const std::string & = "_signal_region", const std::string & = "", const std::string & = "em", const std::string & = "HP", const int & = 0, const int & = 0, const int & = 1, const int & = 0);
 
-void fit_WJetsNormalization_in_Mj_signal_region(RooWorkspace*,  std::map<std::string,int>, const std::string & = "", const std::string & = "", const std::string & = "", const std::string = "", const int  & = 0 , const int & = 0, const float & = 65, const float & = 105);
+void fit_WJetsNormalization_in_Mj_signal_region(RooWorkspace*,  std::map<std::string,int>, const std::string & = "", const std::string & = "", const std::string & = "", const std::string = "", const int  & = 0 , const int & = 0, const float & = 65, const float & = 105, const std::string & = "");
 
-void fit_mlvj_in_Mj_sideband(RooWorkspace*, std::map<std::string,int>, const std::string & = "", const std::string & = "_sb_lo", const std::string & = "", const std::string & = "em", const std::string & = "HP", const std::string & = "", const int & = 0, const int & = 1, const int & = 0);
+void fit_mlvj_in_Mj_sideband(RooWorkspace*, std::map<std::string,int>, const std::string & = "", const std::string & = "_sb_lo", const std::string & = "", const std::string & = "em", const std::string & = "HP", const std::string & = "", const int & = 0, const int & = 1, const int & = 0,const std::string & = "");
 
 void get_WJets_mlvj_correction_sb_lo_to_signal_region(RooWorkspace*, const std::string & = "", const std::string & = "", const std::string & = "_sb_lo", const std::string & = "4fit_", const std::string & = "em", const std::string & = "HP", const int & = 1);
