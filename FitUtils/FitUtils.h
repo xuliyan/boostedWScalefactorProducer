db13e48624f140894ebf7423b2d624cb6af4de6f
Raffaele <raffaele.gerosa@cern.ch>
2014-04-09 20:17:31 +0200

    last updates

--- FitUtils/FitUtils.h
+++ FitUtils/FitUtils.h
@@ -44,6 +44,9 @@ void fit_WJetsNormalization_in_Mj_signal_region(RooWorkspace*,  std::map<std::st
 
 void get_mj_normalization_insignalregion(RooWorkspace*, const std::string & = "", const std::string & = "", const std::string & = "");
 
-void fit_mlvj_in_Mj_sideband(RooWorkspace*, std::map<std::string,int>, const std::string & = "", const std::string & = "_sb_lo", const std::string & = "", const std::string & = "em", const std::string & = "HP", const std::string & = "", const int & = 0, const int & = 1, const int & = 0,const std::string & = "");
+void fit_mlvj_in_Mj_sideband(RooWorkspace*, std::map<std::string,int>, std::map<std::string,std::string>, const std::string & = "", const std::string & = "", const std::string & = "_sb_lo", const std::string & = "em", const std::string & = "HP", const std::string & = "", const int & = 0, const int & = 1, const int & = 0,const std::string & = "");
 
-void get_WJets_mlvj_correction_sb_lo_to_signal_region(RooWorkspace*, const std::string & = "", const std::string & = "", const std::string & = "_sb_lo", const std::string & = "4fit_", const std::string & = "em", const std::string & = "HP", const int & = 1);
+void get_WJets_mlvj_correction_sb_lo_to_signal_region(RooWorkspace*, const std::string & = "", const std::string & = "", const std::string & = "", const std::string & = "_sb_lo", const std::string & = "4fit_", const std::string & = "em", const std::string & = "HP", const int & = 1);
+
+void get_mlvj_normalization_insignalregion(RooWorkspace* ,const std::string & = "", const std::string & = "", const std::string & = "", const std::string & = "", const int & = 0);
+					
