bf20812811b0c7d0ff78e009ec58a6caa017c4bd
Raffaele <raffaele.gerosa@cern.ch>
2014-04-10 22:32:29 +0200

    updates

--- PDFs/MakePdf.h
+++ PDFs/MakePdf.h
@@ -28,6 +28,49 @@
 
 #include "HWWLVJRooPdfs.h"
 
+RooAbsPdf* MakeGeneralPdf(RooWorkspace* ,const std::string & = "", const std::string & = "", const std::string & = "_mj", const std::string & = "em", const std::string & = "HP", RooArgList* = NULL, const int & = 0);
+
+RooExtendPdf* MakeExtendedModel(RooWorkspace*, const std::string & = "", const std::string & = "", const std::string & = "_mj", const std::string & = "em", const std::string & wtagger_label = "HP", RooArgList* = NULL, const int & = 0, const int & = 500);
+
+RooGaussian* addConstraint(RooRealVar*, RooRealVar*, RooRealVar*, std::vector<std::string> &);
+
+void change_dataset_to_histpdf(RooWorkspace*,RooRealVar*,RooDataSet*);
+
+TH1F* change_dataset_to_histogram(RooRealVar*, RooDataSet*, const std::string & = "", const int & = 1);
+
+//////////
+
+RooAbsPdf* get_mj_Model        (RooWorkspace*,const std::string & = "",const std::string & = "em");
+RooAbsPdf* get_General_mj_Model(RooWorkspace*,const std::string & = "",const std::string & = "",const std::string & = "em", const int & = 1);
+RooAbsPdf* get_TTbar_mj_Model  (RooWorkspace*,const std::string & = "_TTbar",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_STop_mj_Model   (RooWorkspace*,const std::string & = "_STop",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_VV_mj_Model     (RooWorkspace*,const std::string & = "_VV",const std::string & model = "", const std::string & = "em", const int & = 1);
+RooAbsPdf* get_WW_EWK_mj_Model (RooWorkspace*,const std::string & = "_WW_EWK",const std::string & model = "", const std::string & = "em", const int & = 1);
+RooAbsPdf* get_WJets_mj_Model  (RooWorkspace*,const std::string & = "_WJets0",const std::string & model = "",const std::string & = "em", const int & = 1, const std::string & = "");
+RooAbsPdf* get_ggH_mj_Model    (RooWorkspace*,const std::string & = "_ggH600",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_vbfH_mj_Model   (RooWorkspace*,const std::string & = "_vbfH600",const std::string & model = "",const std::string & = "em",const int & = 1);
+
+
+RooAbsPdf* get_mlvj_Model        (RooWorkspace*,const std::string & = "",const std::string & = "",const std::string & = "", const std::string & = "em");
+RooAbsPdf* get_General_mlvj_Model(RooWorkspace*,const std::string & = "",const std::string & = "",const std::string & = "",const std::string & = "em", const int & = 1);
+RooAbsPdf* get_TTbar_mlvj_Model  (RooWorkspace*,const std::string & = "_TTbar",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_STop_mlvj_Model   (RooWorkspace*,const std::string & = "_STop",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_VV_mlvj_Model     (RooWorkspace*,const std::string & = "_VV",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_WW_EWK_mlvj_Model (RooWorkspace*,const std::string & = "_WW_EWK",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_WJets_mlvj_Model  (RooWorkspace*, const std::string & = "_WJets0",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_ggH_mlvj_Model    (RooWorkspace*, const std::string & = "_ggH600",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+RooAbsPdf* get_vbfH_mlvj_Model   (RooWorkspace*, const std::string & = "_vbfH600",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
+
+///////////////////////
+void fix_Model(RooWorkspace*, const std::string & = "", const std::string & = "_signal_region", const std::string & = "_mlvj", const std::string & = "",const std::string & = "em", const std::string & = "", const int & = 0);
+
+void fix_Pdf (RooAbsPdf*, RooArgSet*);
+
+void ShowParam_Pdf(RooAbsPdf*,RooArgSet*);
+
+////////////////////////
+void clone_Model(RooWorkspace*, RooAbsPdf*, const std::string & = "", const std::string & = "_signal_region", const std::string & = "_mlvj", const std::string & = "", const std::string & = "em", const int & = 1);
+
 struct SystematicUncertaintyEXO{ const double mean_signal_uncertainty_jet_scale  = 0.013;
                                  const double mean_signal_uncertainty_lep_scale  = 0.001;
                                  const double sigma_signal_uncertainty_jet_scale = 0.033;
@@ -37,7 +80,65 @@ struct SystematicUncertaintyEXO{ const double mean_signal_uncertainty_jet_scale 
                                  const double sigma_mj_smear = 1.11 ;
 };       
 
-struct SystematicUncertaintyHiggs{
+struct SystematicUncertaintyHiggs_01jetBin{
+
+                                  /////// effect on the mean 
+                                  const double mean_signal_uncertainty_jet_scale_ggH_600  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_ggH_700  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_ggH_800  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_ggH_900  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_ggH_1000 = 0.013;
+
+                                  const double mean_signal_uncertainty_jet_scale_vbfH_600  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_vbfH_700  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_vbfH_800  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_vbfH_900  = 0.013;
+                                  const double mean_signal_uncertainty_jet_scale_vbfH_1000 = 0.013;
+
+                                  const double mean_signal_uncertainty_jet_res_ggH_600  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_ggH_700  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_ggH_800  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_ggH_900  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_ggH_1000 = 0.005;
+
+                                  const double mean_signal_uncertainty_jet_res_vbfH_600  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_vbfH_700  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_vbfH_800  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_vbfH_900  = 0.005;
+                                  const double mean_signal_uncertainty_jet_res_vbfH_1000 = 0.005;
+
+
+                                  /////// effect on the sigma 
+                                  const double sigma_signal_uncertainty_jet_scale_ggH_600  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_ggH_700  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_ggH_800  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_ggH_900  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_ggH_1000 = 0.030;
+
+                                  const double sigma_signal_uncertainty_jet_scale_vbfH_600  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_vbfH_700  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_vbfH_800  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_vbfH_900  = 0.030;
+                                  const double sigma_signal_uncertainty_jet_scale_vbfH_1000 = 0.030;
+
+                                  const double sigma_signal_uncertainty_jet_res_ggH_600  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_ggH_700  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_ggH_800  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_ggH_900  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_ggH_1000 = 0.050;
+
+                                  const double sigma_signal_uncertainty_jet_res_vbfH_600  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_vbfH_700  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_vbfH_800  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_vbfH_900  = 0.050;
+                                  const double sigma_signal_uncertainty_jet_res_vbfH_1000 = 0.050;
+
+                                  const double mean_mj_shift = 1.4 ;
+                                  const double sigma_mj_smear = 1.11 ;
+};       
+
+
+struct SystematicUncertaintyHiggs_2jetBin{
 
                                   /////// effect on the mean 
                                   const double mean_signal_uncertainty_jet_scale_ggH_600  = 0.032;
@@ -93,46 +194,3 @@ struct SystematicUncertaintyHiggs{
                                   const double mean_mj_shift = 1.4 ;
                                   const double sigma_mj_smear = 1.11 ;
 };       
-
-RooAbsPdf* MakeGeneralPdf(RooWorkspace* ,const std::string & = "", const std::string & = "", const std::string & = "_mj", const std::string & = "em", const std::string & = "HP", RooArgList* = NULL, const int & = 0);
-
-RooExtendPdf* MakeExtendedModel(RooWorkspace*, const std::string & = "", const std::string & = "", const std::string & = "_mj", const std::string & = "em", const std::string & wtagger_label = "HP", RooArgList* = NULL, const int & = 0, const int & = 500);
-
-RooGaussian* addConstraint(RooRealVar*, RooRealVar*, RooRealVar*, std::vector<std::string> &);
-
-void change_dataset_to_histpdf(RooWorkspace*,RooRealVar*,RooDataSet*);
-
-TH1F* change_dataset_to_histogram(RooRealVar*, RooDataSet*, const std::string & = "", const int & = 1);
-
-//////////
-
-RooAbsPdf* get_mj_Model        (RooWorkspace*,const std::string & = "",const std::string & = "em");
-RooAbsPdf* get_General_mj_Model(RooWorkspace*,const std::string & = "",const std::string & = "",const std::string & = "em", const int & = 1);
-RooAbsPdf* get_TTbar_mj_Model  (RooWorkspace*,const std::string & = "_TTbar",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_STop_mj_Model   (RooWorkspace*,const std::string & = "_STop",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_VV_mj_Model     (RooWorkspace*,const std::string & = "_VV",const std::string & model = "", const std::string & = "em", const int & = 1);
-RooAbsPdf* get_WW_EWK_mj_Model (RooWorkspace*,const std::string & = "_WW_EWK",const std::string & model = "", const std::string & = "em", const int & = 1);
-RooAbsPdf* get_WJets_mj_Model  (RooWorkspace*,const std::string & = "_WJets0",const std::string & model = "",const std::string & = "em", const int & = 1, const std::string & = "");
-RooAbsPdf* get_ggH_mj_Model    (RooWorkspace*,const std::string & = "_ggH600",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_vbfH_mj_Model   (RooWorkspace*,const std::string & = "_vbfH600",const std::string & model = "",const std::string & = "em",const int & = 1);
-
-
-RooAbsPdf* get_mlvj_Model        (RooWorkspace*,const std::string & = "",const std::string & = "",const std::string & = "", const std::string & = "em");
-RooAbsPdf* get_General_mlvj_Model(RooWorkspace*,const std::string & = "",const std::string & = "",const std::string & = "",const std::string & = "em", const int & = 1);
-RooAbsPdf* get_TTbar_mlvj_Model  (RooWorkspace*,const std::string & = "_TTbar",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_STop_mlvj_Model   (RooWorkspace*,const std::string & = "_STop",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_VV_mlvj_Model     (RooWorkspace*,const std::string & = "_VV",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_WW_EWK_mlvj_Model (RooWorkspace*,const std::string & = "_WW_EWK",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_WJets_mlvj_Model  (RooWorkspace*, const std::string & = "_WJets0",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_ggH_mlvj_Model    (RooWorkspace*, const std::string & = "_ggH600",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-RooAbsPdf* get_vbfH_mlvj_Model   (RooWorkspace*, const std::string & = "_vbfH600",const std::string & = "",const std::string & model = "",const std::string & = "em",const int & = 1);
-
-///////////////////////
-void fix_Model(RooWorkspace*, const std::string & = "", const std::string & = "_signal_region", const std::string & = "_mlvj", const std::string & = "",const std::string & = "em", const std::string & = "", const int & = 0);
-
-void fix_Pdf (RooAbsPdf*, RooArgSet*);
-
-void ShowParam_Pdf(RooAbsPdf*,RooArgSet*);
-
-////////////////////////
-void clone_Model(RooWorkspace*, RooAbsPdf*, const std::string & = "", const std::string & = "_signal_region", const std::string & = "_mlvj", const std::string & = "", const std::string & = "em", const int & = 1);
