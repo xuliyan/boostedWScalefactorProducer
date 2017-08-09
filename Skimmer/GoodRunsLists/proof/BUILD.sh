c9a89636b76cb67c68f56ecadba5e324b10a0a3e
thaarres <thea.aarrestad@cern.ch>
2017-08-09 15:28:32 +0200

    Removing Skimmer

--- Skimmer/GoodRunsLists/proof/BUILD.sh
+++
@@ -1,7 +0,0 @@
-
-if [ "$1" = "clean" ]; then
-    make distclean
-    exit 0
-fi
-
-make default
--- Skimmer/GoodRunsLists/proof/SETUP.C
+++
@@ -1,6 +0,0 @@
-int SETUP() {
-
-   if( gSystem->Load( "libGoodRunsLists" ) == -1 ) return -1;
-
-   return 0;
-}
