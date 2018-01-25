6355abf870539d9fed76462570017f5d9b117cf2
Ashley Marie Parker <ash.marie.parker@gmail.com>
2018-01-25 15:33:02 -0500

    Create Pset_nanoSkim.py

---
+++ Skimmer/crab/Pset_nanoSkim.py
@@ -0,0 +1,16 @@
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process('NANO')
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(),
+
+)
+process.source.fileNames = [
+        '/store/user/srappocc/SingleMuon/SingleMuon_Run2016G-07Aug17-v1/180113_045720/0000/test_data_80X_NANO_10.root'
+
+]
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+
+process.options = cms.untracked.PSet()
+
+process.output = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string('80XNanoV0-TTbar_SemiLep.root'), fakeNameForCrab =cms.untracked.bool(True))
+process.out = cms.EndPath(process.output)
