f63ae1979de2c39fbe2fe6b1324f894c5881237e
Ashley Marie Parker <ash.marie.parker@gmail.com>
2018-01-26 12:56:39 -0500

    update to MC conditions

--- Skimmer/crab/crab_script_SFs.py
+++ Skimmer/crab/crab_script_SFs.py
@@ -7,8 +7,12 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputF
 
 from  TTbar_SemiLep import *
 
+# NOTE : This file is configured to Process MC.
+# If you want to process Data then ADD "jsonInput='Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'"
+# to the PostProcessor arguement below
+#
 
-p=PostProcessor(".", inputFiles()  ,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&& ( (nElectron > 0 && HLT_Ele115_CaloIdVT_GsfTrkIdT) || (nMuon > 0 && HLT_Mu50))" ,modules=[TTbar_SemiLep()],provenance=True,fwkJobReport=True,jsonInput='Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt' ,histFileName= 'TTbar_SemiLep_hists.root', histDirName='ttbar_semilep'  , haddFileName=  '80XNanoV0-TTbar_SemiLep.root'  )
+p=PostProcessor(".", inputFiles()  ,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&& ( (nElectron > 0 && HLT_Ele115_CaloIdVT_GsfTrkIdT) || (nMuon > 0 && HLT_Mu50))" ,modules=[TTbar_SemiLep()],provenance=True,fwkJobReport=True ,histFileName= 'TTbar_SemiLep_hists.root', histDirName='ttbar_semilep'  , haddFileName=  '80XNanoV0-TTbar_SemiLep.root'  )
 
 p.run()
 
