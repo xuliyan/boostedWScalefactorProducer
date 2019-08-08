3350ab3cb4197c401cdc9d091baaab44808d1c56
Marc Huwiler <marc.huwiler@cern.ch>
2019-08-08 16:36:51 +0200

    Changed doSkim.py

--- Skimmer/doSkim.py
+++ Skimmer/doSkim.py
@@ -5,7 +5,13 @@ from WTopScalefactorProducer.Skimmer.skimmer import Skimmer
 if len(sys.argv)>1:
    infile = sys.argv[1].split(',')
 else:
-  infile = ["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/00000/0686D954-B442-E811-9AB3-0CC47AD98BC2.root"]
+  infile = [
+#      "root://cms-xrd-global.cern.ch//store/data/Run2018D/SingleMuon/NANOAOD/Nano14Dec2018_ver2-v1/40000/4BF53ABE-BB2D-B147-8BDE-888B21C3E07C.root"
+#      "root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18NanoAODv4/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/120000/BB978C6D-1770-CB42-A45E-AFAA249DAA82.root"
+#      "root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18NanoAODv4/ZZ_TuneCP5_13TeV-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/110000/56817FDC-2D4C-7E4B-ABA0-9A1F7BA505C4.root"
+#      "root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18NanoAODv4/ST_t-channel_top_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/60000/29C0425B-F520-D141-BC3E-8E46A9E88E87.root Skimmed_2019_06_15/ST_t-channel_top_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8 ST_t-channel_top_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8"
+      "root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18NanoAODv4/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/120000/BB978C6D-1770-CB42-A45E-AFAA249DAA82.root"
+  ]
 
 if len(sys.argv)>2: outputDir = sys.argv[2]
 else: outputDir = "Samples"	
@@ -16,24 +22,26 @@ else: name = "test.root"
 if len(sys.argv)>4: chunck = sys.argv[4]
 else: chunck = ""  
 
+#"keep_and_drop.txt"
+#FatJet_msoftdrop>30&&MET_sumEt>40&&    ((nElectron>0&&HLT_Ele115_CaloIdVT_GsfTrkIdT)||
 if infile[0].find("SingleMuon")!=-1:
   channel = "mu"
   print "Processing a Single Muon dataset file..."
-  p=PostProcessor(outputDir, infile,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&&nMuon>0&&HLT_Mu50" ,"keep_and_drop.txt",
+  p=PostProcessor(outputDir, infile, None, None,
                     modules=[Skimmer(channel)],provenance=False,fwkJobReport=False,
-                   jsonInput='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PromptReco/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt')
+                    jsonInput='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt')
                    
 elif infile[0].find("SingleElectron")!=-1:
   channel = "el"
   print "Processing a Single Electron dataset file..."
-  p=PostProcessor(outputDir, infile,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&&nElectron>0&&HLT_Ele115_CaloIdVT_GsfTrkIdT" ,"keep_and_drop.txt",
+  p=PostProcessor(outputDir, infile, None, None,
                     modules=[Skimmer(channel)],provenance=False,fwkJobReport=False,
-                    jsonInput='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PromptReco/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt')
+                    jsonInput='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt')
                     
 else:
   print "Processing MC..."
   channel = "mu"
-  p=PostProcessor(outputDir, infile,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&&((nElectron>0&&HLT_Ele115_CaloIdVT_GsfTrkIdT)||(nMuon>0&&HLT_Mu50))" ,"keep_and_drop.txt",
+  p=PostProcessor(outputDir, infile, "" ,"",
                     modules=[Skimmer(channel)],provenance=False,fwkJobReport=False)
 p.run()
 print "DONE"
